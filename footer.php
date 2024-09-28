4c3e55bf0cb9ca82806cb292a2fae851a57db7d9
Gustavo <gustavoyamamoto@gmail.com>
2024-09-27 19:29:56 -0700

    "Cleaned" footer and deleted sidebar from page.php

--- footer.php
+++ footer.php
@@ -10,20 +10,12 @@
  */
 
 ?>
-
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'portfolio-gustavo' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'portfolio-gustavo' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'portfolio-gustavo' ), 'portfolio-gustavo', '<a href="https://ghyamamoto.com">Gustavo Yamamoto</a>' );
-				?>
+			<?php
+			/* translators: 1: Theme author. */
+			printf( esc_html__( 'Designed by %1$s', 'portfolio-gustavo' ), 'Gustavo Yamamoto' );
+			?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
